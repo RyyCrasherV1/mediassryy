d1facc1dc7ef4fefbcb35c457ad4188ea6654fc5
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2026-01-22 12:19:24 +0700

    Implement basic comment server with Socket.IO

--- varnime-comment-server/server.js
+++ varnime-comment-server/server.js
@@ -1,1 +1,37 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+const users = {};
+
+io.on("connection", socket => {
+
+  socket.on("login", username => {
+    users[socket.id] = { username, count: 0 };
+  });
+
+  socket.on("comment", text => {
+    const user = users[socket.id];
+    if (!user) return;
+
+    user.count++;
+    io.emit("comment", {
+      user: user.username,
+      text
+    });
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Komentar server jalan di :3000");
+});
